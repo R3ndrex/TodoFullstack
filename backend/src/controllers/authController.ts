eff69849e38b1f0d793c6e8e2b6f0beec64f78a5
R3ndrex <yacholohchtoli@gmail.com>
2026-03-19 14:18:53 +0200

    add login function

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -25,11 +25,36 @@ const setTokenCookies = (
 
 export default {
     login: async (req: Request, res: Response) => {
-        const { username, password, email } = req.body;
-        if (!username || !password || !email)
+        if (!ACCESS_SECRET || !REFRESH_SECRET) {
+            throw new Error("Access token secret is not defined");
+        }
+        const { name, password, email } = req.body;
+        if (!name || !password || !email)
             return res.status(400).json({
                 message: "Username and password and email are required",
             });
+
+        const user = await prisma.user.findUnique({ where: { email } });
+        if (!user) {
+            return res.status(401).json({ message: "Invalid credentials" });
+        }
+        const isMatch = bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(401).json({ message: "Invalid credentials" });
+        }
+
+        const accessToken = jwt.sign({ userId: user.id }, ACCESS_SECRET, {
+            expiresIn: "15m",
+        });
+        const refreshToken = jwt.sign({ userId: user.id }, REFRESH_SECRET, {
+            expiresIn: "7d",
+        });
+
+        setTokenCookies(res, accessToken, refreshToken);
+
+        return res
+            .status(200)
+            .json({ id: user.id, name: user.name, email: user.email });
     },
     register: async (req: Request, res: Response) => {
         if (!ACCESS_SECRET || !REFRESH_SECRET) {
