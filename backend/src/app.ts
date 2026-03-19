851172815d067faa6c906fbcf640faa2b5443725
R3ndrex <yacholohchtoli@gmail.com>
2026-03-19 11:51:55 +0200

    add todos, auth routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,12 +2,17 @@ import express from "express";
 import type { Request, Response, NextFunction } from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
+import authRouter from "./routes/auth.js";
+import todoRouter from "./routes/todos.js";
 
 dotenv.config();
 
 const app = express();
 app.use(cors);
 app.use(express.json());
+
+app.use("/api/auth", authRouter);
+app.use("/api/todos", todoRouter);
 
 app.use((_, res) => {
     res.status(404).send("404");
