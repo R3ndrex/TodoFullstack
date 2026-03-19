5752ba1c65c63a20fc4a8e5dc84372a393f14f1b
R3ndrex <yacholohchtoli@gmail.com>
2026-03-19 12:16:16 +0200

    add basic template to controllers

---
+++ backend/src/controllers/authController.ts
@@ -0,0 +1,5 @@
+import type { Request, Response } from "express";
+export default {
+    login: (req: Request, res: Response) => {},
+    register: (req: Request, res: Response) => {},
+};
---
+++ backend/src/controllers/todosController.ts
@@ -0,0 +1,7 @@
+import type { Request, Response } from "express";
+export default {
+    deleteTodo: (req: Request, res: Response) => {},
+    findTodos: (req: Request, res: Response) => {},
+    createTodo: (req: Request, res: Response) => {},
+    updateTodo: (req: Request, res: Response) => {},
+};
