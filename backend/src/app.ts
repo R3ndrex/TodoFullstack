88269aff89deb92c0d478d3f70d2a3d7261e0579
R3ndrex <yacholohchtoli@gmail.com>
2026-03-19 12:33:37 +0200

    add cookie parser

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,11 +4,12 @@ import * as dotenv from "dotenv";
 import cors from "cors";
 import authRouter from "./routes/authRoute.js";
 import todoRouter from "./routes/todosRoute.js";
-
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 const app = express();
 app.use(cors);
+app.use(cookieParser());
 app.use(express.json());
 
 app.use("/api/auth", authRouter);
