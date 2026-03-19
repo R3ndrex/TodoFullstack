c454a85e5026fbc9c1e556a9d2af471908410f2e
R3ndrex <yacholohchtoli@gmail.com>
2026-03-19 14:14:07 +0200

    fix route path to login

--- backend/src/routes/authRoute.ts
+++ backend/src/routes/authRoute.ts
@@ -3,6 +3,6 @@ import authController from "../controllers/authController.js";
 const authRouter = Router();
 
 authRouter.post("/register", authController.register);
-authRouter.post(".login", authController.login);
+authRouter.post("/login", authController.login);
 authRouter.post("/logout", authController.logout);
 export default authRouter;
